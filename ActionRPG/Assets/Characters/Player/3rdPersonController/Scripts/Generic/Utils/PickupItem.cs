3558ff8d329d86ceca6533d266ac48619492c281
Alonzo Machiraju <machthree@gmail.com>
2020-07-25 17:05:54 -0700

    New assets

---
+++ ActionRPG/Assets/Characters/Player/3rdPersonController/Scripts/Generic/Utils/PickupItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    AudioSource audioSource;
+    
+    [SerializeField]
+    AudioClip audioClip;
+    [SerializeField]
+    GameObject particle;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers)
+                r.enabled = false;
+
+            audioSource.PlayOneShot(audioClip);
+            Destroy(gameObject, audioClip.length);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ActionRPG
 A demo of a console-style action-RPG inspired by classic Japanese games such as Threads of Fate.
+
+# Free Unity Asset Credit
+Ferocious Industries (Medieval Props)
+Jean Moreno (Toony Colors)
+Osmann (Medieval Weapons)
+Azerilo (Free Rug Pack)
+Explosive (Crafting Meccanim Animations)
+Meshtint Studios (Polygonal Metalon)
+Ahninniah (Free Game Items)
+Kevin Iglesias (Dance Animations, Basic Motions)
+Ferocious Industries (Medieval Structure Kit)
+Dai Yong Xiang (Arisa, Souta)
+Speedtutor (Full Menu System)
+Invector (Third Person Controller)
